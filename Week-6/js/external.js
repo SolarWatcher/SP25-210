251cf20d351a1b26b62aa63dcf1e20315c138454
Sunny <shadowgamer15237@yahoo.com>
2025-02-18 11:30:35 -0500

    external html

---
+++ Week-6/js/external.js
@@ -0,0 +1,22 @@
+console.log("Ummm.... where are we?")
+
+const birthyear = 1207;
+
+let myName = "Sunny The Sun";
+
+console.log(myName, "was born", birthyear);
+
+myName = "Sunny";
+
+// birthyear = 1000
+
+console.warn(myName, "Perhaps consumed 9374 souls");
+
+console.log("myName is", myName.length, "characters long.");
+
+console.log("myName includes su", myName.toUpperCase().includes("SU"));
+
+myName = "Su"
+
+console.log("myName has uppercase:", myName !== myName.toLowerCase());
+console.log("myName has lowercase:", myName !== myName.toUpperCase());
