60222cf75febf46903b377d7c377114ce6a2af17
Sunny <shadowgamer15237@yahoo.com>
2025-03-07 15:22:01 -0500

    Massive changes to adventure

--- Homework-4/js/adventure.js
+++ Homework-4/js/adventure.js
@@ -71,11 +71,69 @@ function startAdventure() {
 
         adventureRef.innerHTML += "<p>As You look around the room you notice three massive sandstone pillars holding up the roof, Between each pillar is a door.</p>"
 
+        const buttonCreatorThree = [
+            {name: "Rotten Door"},
+            {name: "Runic Door"},
+            {name: "Ruined Door"},
+        ]
+
+        for (let i = 0; i < buttonCreatorThree.length; i++) {
+            const button = buttonCreatorThree[i];
+    
+            const newButton = document.createElement("button");
+            newButton.innerHTML += button.name
+    
+            newButton.dataset.doorName = button.name;
+    
+            newButton.onclick = chooseDoor;
+    
+            adventureRef.appendChild(newButton);
+            
+        }
+
+        function chooseDoor(e) {
+
+            console.log(e.currentTarget);
+            const doorName = e.currentTarget.dataset.doorName;
+    
+            const confirmChoice = confirm("Are you sure you want to enter " + doorName + "?")
+    
+            if(confirmChoice) {
+                adventureRef.innerHTML += "<p>You have entered the " + doorName +"</p>"
+
+                doorChoice = {name: doorName};
+
+                if (doorName == "Rotten Door") {
+                    bodyRef.style.color = "rgb(64, 220, 134)";
+
+                    adventureRef.innerHTML += "<p>Beyond the " + doorName + " lies a simple room covered in mushrooms and vines. As you glance behind you the door has turned into a wall</p>"
+                    adventureRef.innerHTML += "<p>Your mind begins to haze, and your vision goes fuzzy. What do you do?</p>"
+
+                }
+                else if (doorName == "Runic Door") {
+                    bodyRef.style.color = "rgb(112, 245, 223)";
+
+                    adventureRef.innerHTML += "<p>Beyond the " + doorName + " lies a complex room covered in glowing runes that hum at your pressence. As you glance behind you the door has turned into a wall</p>"
+                    adventureRef.innerHTML += "<p>Blue runes appear in the air, they seem to start counting down. What do you do?</p>"
+
+                }
+                else if (doorName == "Ruined Door") {
+                    bodyRef.style.color = "rgb(255, 222, 90)";
+
+                    adventureRef.innerHTML += "<p>Beyond the " + doorName + " lies a broken down room with cracks running through the floors and walls. As you glance behind you the door has turned into a wall</p>"
+                    "<p>As you glance back sand start pouring in from the cracks in the wall, begining to flood the room. What do you do?</p>"
+
+                }
+            }
+    
+        }
+
     }
 
     function sandConsume() {
 
-        adventureRef.style.color = "crimson"
+        adventureRef.style.color = "crimson";
+        bodyRef.style.color = "crimson";
 
         adventureRef.innerHTML += "<p>You give in, laying down and letting the sands slowly pour in, consuming you and burying you forever in these ruins.</p>"
 
