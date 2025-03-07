370ce728bfaf202c7e5407e070738acb062ab072
Sunny <shadowgamer15237@yahoo.com>
2025-03-07 13:27:31 -0500

    Created First Choice

--- Homework-4/js/adventure.js
+++ Homework-4/js/adventure.js
@@ -10,6 +10,7 @@ const startButtonRef = document.getElementById("startButton");
 
 const titleRef = document.getElementById("title");
 
+
 function startAdventure() {
 
     bodyRef.style.backgroundColor = "rgb(67, 67, 67)";
@@ -22,7 +23,63 @@ function startAdventure() {
     startButtonRef.style.backgroundColor = "rgb(212, 0, 255)";
     startButtonRef.innerHTML = "Reset Story";
 
-    adventureRef.innerHTML = "<p>You awaken in an ancient sandstone ruins, underneath the earth, light peering in from the cracks in the roof. Sand slowly seeps in from the cracks, forming small piles.</p>";
-    adventureRef.innerHTML = "<p>As you look around you notice three sandstone pillars, holding up the circular room, A door between each one.</p>";
+    startButtonRef.onclick = reset;
+
+    function reset() {
+
+        window.location.href = 'index.html'
+
+    }
+
+    adventureRef.innerHTML += "<p>You awaken in an ancient sandstone ruins, underneath the earth, light peering in from the cracks in the roof. Sand slowly seeps in from the cracks, forming small piles.</p>"
+
+    const buttonCreatorOne = [
+        "Look Around"
+    ]
+
+    const buttonCreatorTwo = [
+        "Let The Sands Consume You"
+    ]
+
+    for (let i = 0; i < buttonCreatorOne.length; i++) {
+        const button = buttonCreatorOne[i];
+        
+        const newButton = document.createElement("button");
+
+        newButton.innerHTML += button
+
+        newButton.onclick = lookAround;
+
+        adventureRef.appendChild(newButton);
+
+    }
+
+    for (let i = 0; i < buttonCreatorTwo.length; i++) {
+        const button = buttonCreatorTwo[i];
+        
+        const newButton = document.createElement("button");
+
+        newButton.innerHTML += button
+
+        newButton.onclick = sandConsume;
+
+        adventureRef.appendChild(newButton);
+
+    }
+
+    function lookAround() {
+
+        adventureRef.innerHTML += "<p>As You look around the room you notice three massive sandstone pillars holding up the roof, Between each pillar is a door.</p>"
+
+    }
+
+    function sandConsume() {
+
+        adventureRef.style.color = "crimson"
+
+        adventureRef.innerHTML += "<p>You give in, laying down and letting the sands slowly pour in, consuming you and burying you forever in these ruins.</p>"
+
+        adventureRef.innerHTML += "<p>Your Adventure Comes To An End.</p>"
+    }
 
 }
