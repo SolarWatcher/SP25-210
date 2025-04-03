170f1ab49cef9197f9a81041609e314a979d7ae3
Sunny <shadowgamer15237@yahoo.com>
2025-04-03 11:40:37 -0400

    Edited week 12

--- Week-12/js/home.js
+++ Week-12/js/home.js
@@ -1,17 +1,22 @@
 import { app } from "./app.js";
 
-import {getFirestore, collection, getDocs} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js"
+import {getFirestore, collection, getDocs, addDoc} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js"
 
 const db = getFirestore(app);
 
 const dreamsCollection = collection(db, "hopesdreams");
 
+const dreamsRef = document.querySelector("#dreams");
+
+const dreamFormRef = document.querySelector("#new-dream");
+
+const dreamTextRef = document.querySelector("#dream-text");
+
 async function getDreams() {
 const dreamsDocs = await getDocs(dreamsCollection);
 console.log(dreamsDocs.docs.length);
 
-const dreamsRef = document.querySelector("#dreams");
-
+dreamsRef.innerHTML = ""
 for (let i = 0; i < dreamsDocs.docs.length; i++) {
     const currentDream = dreamsDocs.docs[i];
     console.log(currentDream.id, currentDream.data());
@@ -20,5 +25,22 @@ for (let i = 0; i < dreamsDocs.docs.length; i++) {
     
 }
 }
+
+async function addNewDream(e) {
+    e.preventDefault();
+    // prevents the default thing from happening.
+
+    const dreamTextValue = dreamTextRef.value;
+
+    const newDream = await addDoc(dreamsCollection, { Dreams: dreamTextValue });
+
+    console.log(newDream);
+
+    getDreams();
+
+    dreamFormRef.reset();
+}
+
+dreamFormRef.onsubmit = addNewDream;
 
 getDreams();
